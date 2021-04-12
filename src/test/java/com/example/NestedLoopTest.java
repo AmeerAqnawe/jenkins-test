50e0098ddfe2ec72895b2f534eb5999d269b0ce7
Philipp Straubinger <philipp.straubinger@uni-passau.de>
2021-04-12 11:03:35 +0200

    Remove: Unnecessary methods and test for NestedLoop

--- src/test/java/com/example/NestedLoopTest.java
+++ src/test/java/com/example/NestedLoopTest.java
@@ -6,15 +6,4 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class NestedLoopTest {
 
-    @Test
-    void nestedForLoops() {
-        NestedLoop loop = new NestedLoop();
-        assertEquals(56, loop.nestedForLoops());
-    }
-
-    @Test
-    void nestedWhileForLoops() {
-        NestedLoop loop = new NestedLoop();
-        assertEquals(56, loop.nestedWhileForLoops());
-    }
 }
