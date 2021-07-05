1f51c3060d975efd5f44a828b4faa8069be0c43d
Philipp Straubinger <philipp.straubinger@uni-passau.de>
2021-07-05 13:31:50 +0200

    Add: Test for Rational of()

--- src/test/java/com/example/RationalTest.java
+++ src/test/java/com/example/RationalTest.java
@@ -2,6 +2,8 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigInteger;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RationalTest {
@@ -11,5 +13,12 @@ class RationalTest {
         Rational rational1 = Rational.of(3);
         Rational rational2 = Rational.of(3);
         assertEquals(rational2, rational1);
+    }
+
+    @Test
+    void testOf() {
+        BigInteger numerator = BigInteger.ONE;
+        BigInteger denominator = BigInteger.ZERO;
+        assertThrows(IllegalArgumentException.class, () -> Rational.of(numerator, denominator));
     }
 }
